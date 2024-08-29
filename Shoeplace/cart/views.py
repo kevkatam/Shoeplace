98f3b0082fffb9f995a92cc5bdee70eaaee5e193
kevkatam <kevkatam@gmail.com>
2024-08-29 22:31:20 +0300

    adding views for cart model

--- Shoeplace/cart/views.py
+++ Shoeplace/cart/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
+from .models import Cart, CartItem
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+
 
 # Create your views here.
+@require_POST
+def cart_add(request, product_id):
+    """ view to add a product to a cart """
+    cart_ide = request_session.get('cart_id')
+
+    if cart_id:
+        try:
+            cart = Cart.objects.get(id=cart_id)
+        except Cart.DoesNotExist:
+            cart = Cart.objects.create()
+    else:
+        cart = Cart.objects.create()
+        request.session['cart_id'] = cart.id
+
+    product = get_object_or_404(Product, id=product_id)
+
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+
+    if not created:
+        cart_item.quantity += 1
+
+    cart_item.save()
+
+    response_data = {
+        "success": True,
+        "message": f'Added {product.name} to cart'
+    }
+
+    return JsonResponse(response_data)
+
+def cart_detail(request):
+    """ view that shows the cart """
+    cart_id = request.session.get('cart_id')
+
